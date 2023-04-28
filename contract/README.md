6c12bff411d0a4cca2bd85b294c3c832eea977e1
aman035 <guptaaman200115@gmail.com>
2023-04-28 22:41:03 +0530

    Contract update

--- contract/README.md
+++ contract/README.md
@@ -5,9 +5,8 @@
 Factory contract for deploying exam Contract
 
 Deploying contracts with the account: 0x28F1C7B4596D9db14f85c04DcBd867Bf4b14b811
-Account balance: 1922526971822457989
 Network: Polygon Mumbai
-Exam Factory address: 0xC7B2183fCA9Fd2931a5C417a5281Dd00e2Bf8146
+Exam Factory address: 0x9971ae44Bd0d4bc3150D7C8944b365718867418F
 
 ### Exam
 
--- contract/contracts/Exam.sol
+++ contract/contracts/Exam.sol
@@ -4,21 +4,29 @@ pragma solidity ^0.8.9;
 /// @author Aman Gupta
 /// @title Exam Contract
 contract Exam {
-    address public creator;
+    address public creator; // exam creator
     address public servicer; // account responsible for updating data on behalf of a student
     string public examData; // IPFS Hash
     uint256 public startTime; // Exam startTime
     uint256 public endTime; // Exam endTime
 
     struct student {
-        uint id;
-        string meta;
-        string sheetHash;
-        string sheet;
+        address wallet; // wallet address
+        string meta; // other meta details
+        string sheetHash; // sha256 Hash of sheet
+        string sheet; // ipfs hash of actual sheet
     }
     student[] public enrolledStudents;
+    mapping(address => bool) public isEnrolled;
+    mapping(address => uint) public studentId;
 
-    constructor(address _creator, address _servicer, string memory _examData, uint256 _startTime, uint256 _endTime) {
+    constructor(
+        address _creator,
+        address _servicer,
+        string memory _examData,
+        uint256 _startTime,
+        uint256 _endTime
+    ) {
         creator = _creator;
         servicer = _servicer;
         examData = _examData;
@@ -52,14 +60,27 @@ contract Exam {
         _;
     }
 
-    modifier isEnrolled(uint _studentId) {
-        require(enrolledStudents.length > _studentId , "Student is not enrolled");
+    modifier afterDeadline() {
+        require(block.timestamp >= endTime, "Exam has not ended yet!");
+        _;
+    }
+
+    modifier onlyEnrolled() {
+        require(isEnrolled[msg.sender], "You are not enrolled in this exam!");
+        _;
+    }
+
+    modifier onlyNotEnrolled() {
+        require(
+            !isEnrolled[msg.sender],
+            "You are already enrolled in this exam!"
+        );
         _;
     }
 
     /// @notice modify the servicer account to provide flexibility if creator wants some other acount to handle data editing
     /// @param _servicer new servicer address
-    function modifyServicer(address _servicer) public onlyCreator{
+    function modifyServicer(address _servicer) public onlyCreator {
         servicer = _servicer;
     }
 
@@ -79,30 +100,42 @@ contract Exam {
 
     /// @notice enroll a new student
     /// @param _studentMeta student details
-    function enroll(string memory _studentMeta) public restricted beforeDeadline {
+    function enroll(
+        string memory _studentMeta
+    ) public beforeDeadline onlyNotEnrolled {
         student memory newEnrollment;
-        newEnrollment.id = enrolledStudents.length;
+        newEnrollment.wallet = msg.sender;
         newEnrollment.meta = _studentMeta;
+
+        isEnrolled[msg.sender] = true;
+        studentId[msg.sender] = enrolledStudents.length;
+
         enrolledStudents.push(newEnrollment);
     }
 
-    /// @notice add sheet hash before exam deadline
-    /// @param _studentId address of student
+    /// @notice add sheet hash before exam deadline - Done By Servicer
+    /// @param _wallet address of student
     /// @param _sheetHash Hash of answer sheet
-    function addSheetHash(uint _studentId, string memory _sheetHash) public restricted isEnrolled(_studentId) examTime {
-        enrolledStudents[_studentId].sheetHash = _sheetHash;
+    function addSheetHash(
+        address _wallet,
+        string memory _sheetHash
+    ) public restricted examTime {
+        require(isEnrolled[_wallet], "Student not enrolled in this exam!");
+
+        uint id = studentId[_wallet];
+        enrolledStudents[id].sheetHash = _sheetHash;
     }
 
     /// @notice Add the actual answer sheet
-    /// @param _studentId address of student
     /// @param _sheet IPFS hash of answer sheet file
-    function addSheet(uint _studentId, string memory _sheet) public restricted isEnrolled(_studentId) examStarted {
-        enrolledStudents[_studentId].sheet = _sheet;
+    function addSheet(string memory _sheet) public onlyEnrolled afterDeadline {
+        uint id = studentId[msg.sender];
+        enrolledStudents[id].sheet = _sheet;
     }
 
     /// @notice get details of all enrolled students
     /// @return array of type struct
-    function getEnrolledStudents() public view returns(student[] memory){
+    function getEnrolledStudents() public view returns (student[] memory) {
         return enrolledStudents;
     }
 }
