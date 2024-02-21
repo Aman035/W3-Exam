4626ce86e6b121c79da84a0132ddef8cdf0d8255
aman035 <guptaaman200115@gmail.com>
2024-02-22 00:46:37 +0530

    fix: exams Page

--- client/src/pages/Exams/Exams.tsx
+++ client/src/pages/Exams/Exams.tsx
@@ -1,89 +1,52 @@
-import React, { useEffect, useState } from "react";
-import "react-dropzone-uploader/dist/styles.css";
-import "./Exams.scss";
-import { useContractRead } from "wagmi";
-import { config } from "../../config";
-import "react-datetime/css/react-datetime.css";
-import Toast from "../../components/Toast/Toast";
-import { readContracts } from "wagmi";
-import { CIDString, Web3Storage } from "web3.storage";
-import Card from "../../components/Card/Card";
+import React, { useEffect, useState } from 'react'
+import 'react-dropzone-uploader/dist/styles.css'
+import './Exams.scss'
+import { useContractRead } from 'wagmi'
+import { config } from '../../config'
+import 'react-datetime/css/react-datetime.css'
+import Toast from '../../components/Toast/Toast'
+import Card from '../../components/Card/Card'
+import axios from 'axios'
+import Loader from '../../components/Loader/Loader'
 
-function Exams() {
-  const [loading, setLoading] = useState(true);
-  const [examAddress, setExamAddress] = useState<any>([]);
-  const [examData, setExamData] = useState<any>([]);
+interface ExamData {
+  address: `0x${string}`
+  creator: `0x${string}`
+  name: string
+  info: string
+  startTime: { _hex: string }
+  endTime: { _hex: string }
+}
+
+const Exams = () => {
   const [toast, setToast] = useState<{
-    message: string;
-    type: "error" | "success" | "warning";
-  } | null>(null);
+    message: string
+    type: 'error' | 'success' | 'warning'
+  } | null>(null)
 
   const closeToast = () => {
-    setToast(null);
-  };
+    setToast(null)
+  }
 
-  const { data, isError, isLoading } = useContractRead({
+  const {
+    data: examsAddress,
+    // isError,
+    isLoading,
+  } = useContractRead({
     address: config.examFactoryAddress as `0x${string}`,
     abi: config.examFactoryABI,
-    functionName: "getDeplyedExams",
-  });
+    functionName: 'getDeplyedExams',
+    args: [],
+  })
 
-  useEffect(() => {
-    setExamAddress(data);
-    setLoading(isLoading);
-    if (isError) {
-      setToast({
-        message: "Unable To Get Exam Data !!!",
-        type: "error",
-      });
-    }
-
-    examAddress.map(async (each: `0x${string}`) => {
-      const examContract = {
-        address: each,
-        abi: config.examABI,
-      };
-      const examDetails = await readContracts({
-        contracts: [
-          {
-            ...examContract,
-            functionName: "creator",
-          },
-          {
-            ...examContract,
-            functionName: "examData",
-          },
-          {
-            ...examContract,
-            functionName: "startTime",
-          },
-          {
-            ...examContract,
-            functionName: "endTime",
-          },
-        ],
-      });
-      const client = new Web3Storage({
-        token: process.env.REACT_APP_WEB3_STORAGE_TOKEN!,
-      });
-      const res = await client.get(examDetails[1] as CIDString);
-      const files = await res?.files();
-      const examMeta = await files![0].arrayBuffer();
-      const uint8Array = new Uint8Array(examMeta);
-      const jsonStr = new TextDecoder().decode(uint8Array);
-      const jsonObj = JSON.parse(jsonStr);
-      const data = {
-        address: each,
-        creator: examDetails[0],
-        name: jsonObj.name,
-        info: jsonObj.info,
-        examSheet: files![1],
-        startTime: examDetails[2],
-        endTime: examDetails[3],
-      };
-      setExamData((prevState: any) => [data, ...prevState]);
-    });
-  }, [examAddress]);
+  // useEffect(() => {
+  //   if (isError) {
+  //     setToast({
+  //       message: 'Unable To Get Exams !!!',
+  //       type: 'error',
+  //     })
+  //   }
+  // }, [isError])
 
   return (
     <div className="container">
@@ -91,14 +54,77 @@ function Exams() {
         <h1 className="title">Exams</h1>
         <h3 className="info">You can browse all the created Exams</h3>
       </div>
-      {examData.map((exam: any) => (
-        <Card key={exam.address} {...exam} />
-      ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {(examsAddress as `0x${string}`[]).map((examAddress) => (
+            <ExamCard key={examAddress} examAddress={examAddress} />
+          ))}
+        </>
+      )}
       {toast && (
         <Toast message={toast.message} type={toast.type} onClose={closeToast} />
       )}
     </div>
-  );
+  )
 }
 
-export default Exams;
+const ExamCard = ({ examAddress }: { examAddress: `0x${string}` }) => {
+  const [examData, setExamData] = useState<ExamData | null>(null)
+
+  const examContract = {
+    address: examAddress,
+    abi: config.examABI,
+  }
+
+  const { data: creator } = useContractRead({
+    ...examContract,
+    functionName: 'creator',
+    args: [],
+  })
+  const { data: examMetaDataLink } = useContractRead({
+    ...examContract,
+    functionName: 'examData',
+    args: [],
+  })
+  const { data: startTime } = useContractRead({
+    ...examContract,
+    functionName: 'startTime',
+    args: [],
+  })
+  const { data: endTime } = useContractRead({
+    ...examContract,
+    functionName: 'endTime',
+    args: [],
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (examMetaDataLink) {
+        try {
+          const examMetaData = (
+            await axios.get(`${examMetaDataLink}/examDetails`)
+          ).data
+
+          const data: ExamData = {
+            address: examAddress,
+            creator: creator as `0x${string}`,
+            name: examMetaData.name as string,
+            info: examMetaData.info as string,
+            startTime: startTime as { _hex: string },
+            endTime: endTime as { _hex: string },
+          }
+          setExamData(data)
+        } catch (error) {
+          console.error('Error fetching exam metadata:')
+        }
+      }
+    }
+    fetchData()
+  }, [creator, examData, startTime, endTime, examAddress, examMetaDataLink])
+
+  return <div>{examData && <Card {...examData} />}</div>
+}
+
+export default Exams
